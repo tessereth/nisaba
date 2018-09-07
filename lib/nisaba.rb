ac3c466646907f450528a821575e62941a54cb1d
tessereth <tessereth@yahoo.com.au>
2018-09-08 09:20:08 +1000

    Add basic sinatra web app

--- lib/nisaba.rb
+++ lib/nisaba.rb
@@ -1,5 +1,99 @@
+require 'sinatra'
+
 require "nisaba/version"
 
 module Nisaba
-  # Your code goes here...
+  extend self
+  extend Forwardable
+
+  def_delegators :'Nisaba::Application', :configure, :run!
+
+  class Application
+    def self.configure
+      @config ||= Configuration.new
+      yield @config
+    end
+
+    def self.handle(payload)
+      @config.handlers.each do |handler|
+        handler.handle(payload)
+      end
+    end
+
+    def self.run!
+      Sinatra::Application.get '/*' do
+        # TODO: remove
+        Nisaba::Application.handle(test: :data)
+        'OK'
+      end
+
+      Sinatra::Application.post '/webhook' do
+        request.body.rewind
+        payload_body = request.body.read
+        halt 401, "Signatures didn't match!" unless verify_signature(payload_body)
+
+        payload = JSON.parse(params[:payload])
+        Nisaba::Application.handle(payload)
+      end
+
+      Sinatra::Application.run!
+    end
+
+    def self.verify_signature(payload_body)
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), @config.webhook_secret, payload_body)
+      Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    end
+  end
+
+  class Configuration
+    attr_accessor :api_token, :webhook_secret, :handlers
+
+    def initialize
+      self.handlers = []
+    end
+
+    def label(name, &block)
+      puts "Managing label '#{name}'"
+      handlers << Handler::Label.new(name, block)
+    end
+
+    def comment(name, &block)
+      puts "Managing comment '#{name}'"
+      handlers << Handler::Comment.new(name, block)
+    end
+
+    def review(name, &block)
+      puts "Managing review '#{name}'"
+      handlers << Handler::Review.new(name, block)
+    end
+  end
+
+  module Handler
+    class Base
+      attr_reader :name, :block
+
+      def initialize(name, block)
+        @name = name
+        @block = block
+      end
+    end
+
+    class Label < Base
+      def handle(payload)
+        puts "Reconciling label '#{name}' for payload #{payload}"
+      end
+    end
+
+    class Comment < Base
+      def handle(payload)
+        puts "Reconciling comment '#{name}' for payload #{payload}"
+      end
+    end
+
+    class Review < Base
+      def handle(payload)
+        puts "Reconciling review '#{name}' for payload #{payload}"
+      end
+    end
+  end
 end
--- nisaba.gemspec
+++ nisaba.gemspec
@@ -6,12 +6,12 @@ require "nisaba/version"
 Gem::Specification.new do |spec|
   spec.name          = "nisaba"
   spec.version       = Nisaba::VERSION
-  spec.authors       = ["TODO: Write your name"]
-  spec.email         = ["TODO: Write your email address"]
+  spec.authors       = ["tessereth"]
+  # spec.email         = ["tesser"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Github PR management DSL}
+  spec.description   = %q{Automate the boring bits of managing github pull requests quickly and easily with your own custom rules.}
+  spec.homepage      = "https://github.com/tessereth/nisaba"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -32,6 +32,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency 'sinatra'
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
